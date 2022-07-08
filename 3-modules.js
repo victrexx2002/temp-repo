a04f6ca8eabc6abb62dbf27758be367d5baafe75
victrexx2002 <victrexxmgbemena@gmail.com>
2022-07-08 02:28:18 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//CommonJS,every file is module(by default)
+// Modules - Encapsulated Code (only share minimum)
+const {john, peter} = require('./4-names');
+const sayHi= require('./5-utils');
+const data = require('./6-alternative-flavor')
+console.log(data);
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(john);
+sayHi(peter);
---
+++ 8-os-module.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+//info about current user
+const user = os.userInfo();
+console.log(user);
+
+// method to return the system uptime in seconds
+
+console.log(`the systems uptime is ${os.uptime()} seconds`)
+
+
+const currentOS = {
+    name : os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem : os.freemem()
+}
+
+console.log(currentOS);
